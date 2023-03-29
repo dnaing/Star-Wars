3e392f988b212f212b43553d9421c381c16f5fa1
Derek Naing <dereknaing01@gmail.com>
2023-03-28 19:19:31 -0700

    Added navbar and homescreen routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Pages/Home/Home";
+import Films from './components/Pages/Films';
+import People from './components/Pages/People';
+import Species from './components/Pages/Species';
+import Planets from './components/Pages/Planets';
+import Starships from './components/Pages/Starships';
+import Vehicles from './components/Pages/Vehicles';
+
+
 import './App.css';
+
+
+
+import "./fonts/Starjedi.ttf";
+import "./fonts/Starjhol.ttf";
+import "./fonts/FranklinGothic.otf";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/films' element={<Films/>} />
+          <Route path='/people' element={<People/>} />
+          <Route path='/species' element={<Species/>} />
+          <Route path='/planets' element={<Planets/>} />
+          <Route path='/starships' element={<Starships/>} />
+          <Route path='/vehicles' element={<Vehicles/>} />
+        </Routes>
+      </Router>
+    </div> 
+    
   );
 }
 

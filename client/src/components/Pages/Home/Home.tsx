20c021863f060331bb3a256c24576124edc0c821
Derek Naing <dereknaing01@gmail.com>
2023-03-28 20:57:11 -0700

    Added placeholder hero images in home screen

--- client/src/components/Pages/Home/Home.tsx
+++ client/src/components/Pages/Home/Home.tsx
@@ -3,13 +3,22 @@ import React from 'react';
 import WelcomeSection from '../../WelcomeSection/WelcomeSection';
 
 import '../../Navbar/Navbar.css';
+import './Home.css';
+
+import welcomeImage from '../../../assets/images/welcomeImage.jpg';
 
 
 function Home() {
   return (
     <>
       <div className = 'body'>
-        <WelcomeSection/>
+        {/* <WelcomeSection/> */}
+
+        <div className='hero-container'>
+          <img className='welcome-hero' src={welcomeImage} alt='Welcome Hero Image'/>
+          <img className='welcome-hero-2' src={welcomeImage} alt='Welcome Hero Image'/>
+        </div>
+
       </div>
     </>
   );

88bff2e409ffc804b4cdf61d56bd673e45d9fd4e
Derek Naing <dereknaing01@gmail.com>
2023-03-31 04:49:17 -0700

    All hero banners added

--- client/src/components/Pages/Home/Home.tsx
+++ client/src/components/Pages/Home/Home.tsx
@@ -8,8 +8,11 @@ import './Home.css';
 
 import welcomeImage from '../../../assets/images/starwars.jpg';
 import filmsImage from '../../../assets/images/films.jpg';
-import peopleImage from '../../../assets/images/homeBanner.jpg';
-
+import peopleImage from '../../../assets/images/people.jpg';
+import speciesImage from '../../../assets/images/species.jpg';
+import planetsImage from '../../../assets/images/planets.jpg';
+import starshipsImage from '../../../assets/images/starships.jpg';
+import vehiclesImage from '../../../assets/images/vehicles.jpg';
 
 function scrollToContent() {
   const filmsHero = document.getElementById('startOfContent')
@@ -45,7 +48,27 @@ function Home() {
 
           {/* Container for the People hero banner */}
           <div>
-            <img className='people-hero' src={peopleImage} alt='Films Hero Image'/>
+            <img className='people-hero' src={peopleImage} alt='People Hero Image'/>
+          </div>
+
+          {/* Container for the Species hero banner */}
+          <div>
+            <img className='species-hero' src={speciesImage} alt='Species Hero Image'/>
+          </div>
+
+          {/* Container for the Planets hero banner */}
+          <div>
+            <img className='planets-hero' src={planetsImage} alt='Planets Hero Image'/>
+          </div>
+
+          {/* Container for the Starships hero banner */}
+          <div>
+            <img className='starships-hero' src={starshipsImage} alt='Starships Hero Image'/>
+          </div>
+
+          {/* Container for the Vehicles hero banner */}
+          <div>
+            <img className='vehicles-hero' src={vehiclesImage} alt='Vehicles Hero Image'/>
           </div>
           
           

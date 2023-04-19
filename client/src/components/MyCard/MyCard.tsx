22ea3267fc1519f47957dcbf8b441d4b82bd53e2
Derek Naing <dereknaing01@gmail.com>
2023-04-19 15:34:28 -0700

    Added console logs for if a film card is clicked

--- client/src/components/MyCard/MyCard.tsx
+++ client/src/components/MyCard/MyCard.tsx
@@ -3,7 +3,9 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import { CardActionArea } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import './MyCard.css';
+
 
 interface Props {
     imageURL: string;
@@ -13,13 +15,13 @@ interface Props {
 
 
 function MyCard(props: Props) {
-
+  
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
           const classList = entry.target.classList;
-          console.log(entry);
-          console.log(classList);
+          // console.log(entry);
+          // console.log(classList);
           if (entry.isIntersecting) { 
               entry.target.classList.add('materialize');
           } 
@@ -28,15 +30,20 @@ function MyCard(props: Props) {
           }
       });
     });
-  
+
     const hiddenElements = document.querySelectorAll('.hidden-2');
     hiddenElements.forEach((element) => observer.observe(element));
   }, []);
 
+  const navigate = useNavigate();
+  function handleClick(cardName: string) {
+      console.log("Card clicked with name of " + cardName);
+  }
+
   return (
     <>
       <div className='card-container hidden-2'>
-        <Card style={{ maxWidth: "70%", maxHeight: "100%", margin: "auto" }}>
+        <Card onClick={() => handleClick(props.name)} style={{ maxWidth: "70%", maxHeight: "100%", margin: "auto" }}>
           
           <CardActionArea>
             <CardMedia

fab7e6ef96e369c0be12152d00dc5d8a41476c8f
Derek Naing <dereknaing01@gmail.com>
2023-05-03 16:46:11 -0700

    Added planet images

--- client/src/components/Pages/Planets/Planets.tsx
+++ client/src/components/Pages/Planets/Planets.tsx
@@ -117,10 +117,10 @@ function Planets() {
 
 
         <div className='planet-cards-grid'>
-          <Grid container rowSpacing={{ xs: 5, sm: 5, md: 10 }} columnSpacing={{ xs: 1, sm: 1, md: 1 }} columns={{ xs: 4, sm: 8, md: 16 }} >
+          <Grid container rowSpacing={{ xs: 2.5, sm: 2.5, md: 5 }} columnSpacing={{ xs: 1, sm: 1, md: 1 }} columns={{ xs: 4, sm: 8, md: 20 }} >
               {Array.from(planetData).map((planetItem, index) => (
               <Grid item xs={2} sm={4} md={4} key={planetItem._id}>
-                  <GeneralCard {...{object: planetItem, imageURL: "https://storage.cloud.google.com/starwars_people_imgs/luke" + "" + ".jpg", type:"planets"}} />
+                  <GeneralCard {...{object: planetItem, imageURL: "https://storage.cloud.google.com/starwars_planets_imgs/" + planetItem.name.replace(/\s+/g, '') + ".jpg", type:"planets"}} />
               </Grid>
           ))}
           </Grid>   

f4960c1873a02dede5ce1ae44ea21667b3e72a18
Derek Naing <dereknaing01@gmail.com>
2023-06-13 19:12:30 -0700

    Fixed hostname missing http

--- client/src/components/Variables/variables.tsx
+++ client/src/components/Variables/variables.tsx
@@ -1,5 +1,5 @@
 // let hostName = "https://35.236.104.1:8443";
-let hostName = "backend.thestarwarscodex.com:8080";
+let hostName = "http://backend.thestarwarscodex.com:8080";
 // for local testing set it to localhost:4000
 
 export default hostName;

77c0f2bcbbdd7d268e6f40dbdef7033c6d8614d4
Derek Naing <dereknaing01@gmail.com>
2023-06-16 18:00:54 -0700

    configured client to look for server at https on port 8443

--- client/src/components/Variables/variables.tsx
+++ client/src/components/Variables/variables.tsx
@@ -1,5 +1,5 @@
 // let hostName = "https://35.236.104.1:8443";
-let hostName = "http://backend.thestarwarscodex.com:8080";
+let hostName = "https://backend.thestarwarscodex.com:8443";
 // for local testing set it to localhost:4000
 
 export default hostName;
